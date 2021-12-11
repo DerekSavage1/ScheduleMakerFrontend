7d50b2574330c0db053d4e5980e4c5c6982aa1dc
dereksavage1 <d.savag1@gmail.com>
2021-12-10 21:11:04 -0800

    schedule mapping

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
-  constructor() { }
+  buttonType: string;
+  toggle: boolean;
+
+  constructor() {
+    this.buttonType = 'btn-outline-secondary';
+    this.toggle = false;
+   }
 
   ngOnInit(): void {
+    
   }
 
+  onClickSchedule(): void {
+    if(!this.toggle)
+      this.buttonType =  'btn-primary';
+    else
+      this.buttonType = 'btn-outline-secondary';
+    this.toggle = !this.toggle;
+  }
 }
